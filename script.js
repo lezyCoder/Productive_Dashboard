b65e49a214fc408d9b5c6cdc2f8eabe0ec63776b
lezyCoder <iamvijaysingh23@gmail.com>
2025-08-13 06:03:25 +0530

    Updated index.html, script.js, and style.css

--- script.js
+++ script.js
@@ -10,7 +10,33 @@ allElems.forEach((elem) => {
 
 fullElemPageBackBtn.forEach((back) => {
   back.addEventListener("click", () => {
-
-       fullElemPage[back.id].style.display = "none";
+    fullElemPage[back.id].style.display = "none";
   });
+});
+
+// Todo Page Events
+
+const form = document.querySelector(".addTask form");
+const input = document.querySelector(".addTask form input");
+const taskDetailsInput = document.querySelector(".addTask form textarea");
+const taskCheckbox = document.querySelector(".addTask form #check");
+const allTask = document.querySelector(".allTask");
+
+let currentTask = [
+  {
+    task: "Mandir Jao",
+    details: "Mahadev ke Mandir Jaao",
+    imp: true,
+  },
+  {
+    task: "Gym Jao",
+    details: " 2 Hours Exercise karo",
+    imp: true,
+  },
+];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  document.querySelector(".allTask");
 });
